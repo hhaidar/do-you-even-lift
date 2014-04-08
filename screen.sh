02568119a6119db4b4c64ba4bd312f84c9d1430f
Raspberry <raspberry@example.com>
2014-04-08 23:09:43 +0000

    Move to better system files

--- screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LOGFILE='/tmp/debug.log'
-#LOGFILE='/dev/null'
-
-export DISPLAY=:0
-nohup matchbox-window-manager >> $LOGFILE &
-nohup npm start >> $LOGFILE &
-nohup chromium-browser --kiosk http://localhost:5000/ >> $LOGFILE &
