36e19f622b5e23bfe6592066f195653740ad82f7
Raspberry Pi <pi@example.com>
2014-04-09 05:35:05 +0000

    Fix log

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ LOGFILE='/home/pi/your-face-masterpiece.log'
 cd /home/pi/your-face-masterpiece
 if /usr/bin/git pull | grep -vq 'Already up-to-date.'; then
   killall -HUP chromium >> $LOGFILE
-  echo "Updated from Git at `date`"
- /home/pi/your-face-masterpiece/deploy.sh
+  echo "Updated from Git at `date`" >> $LOGFILE
+ /home/pi/your-face-masterpiece/deploy.sh >> $LOGFILE
 fi
