d88535d3cf3c67654283e50a32e798453fb619d3
Houssam Haidar <houssam@sdelements.com>
2014-04-09 01:37:08 -0400

    Fix broken people, trying midori again

---
+++ assets/js/data/people.js
@@ -0,0 +1,54 @@
+(function(window) {
+    window.data = window.data || {};
+    window.data.people = [
+        {
+            "id": "wolever",
+            "firstname": "David",
+            "lastname": "Wolever",
+            "title": "Director, PyCon Canada",
+            "twitter": "@wolever",
+            "github": "wolever",
+            "picture": "david.jpg"
+        },
+        {
+            "id": "haidar",
+            "firstname": "Houssam",
+            "lastname": "Haidar",
+            "title": "Chicken dinner or somethin'",
+            "twitter": "@letstweetbro",
+            "github": "hhaidar",
+            "picture": "houssam.jpg"
+        },
+        {
+            "id": "jarl",
+            "firstname": "Jarl",
+            "lastname": "Stefansson",
+            "title": "Dev-ops Jesus",
+            "github": "jalli",
+            "picture": "jarl.jpg",
+            "scale": {
+                "lastname": 0.7
+            }
+        },
+        {
+            "id": "ram",
+            "firstname": "Ramanan",
+            "lastname": "Sivaranjan",
+            "title": "Lead Developer, SDE",
+            "github": "funkaoshi",
+            "twitter": "funkaoshi",
+            "picture": "ram.jpg",
+            "scale": {
+                "lastname": 0.7
+            }
+        },
+        {
+            "id": "anonymoose",
+            "firstname": "Anony",
+            "lastname": "Moose",
+            "title": "Who's asking?",
+            "github": "???",
+            "twitter": "???"
+        }
+    ];
+})(window);
