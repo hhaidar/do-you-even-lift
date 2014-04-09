9e0c38d73c9a2a035cb8c4959c388ca64c13235d
Jarl Stefansson <jarl@sdelements.com>
2014-04-09 01:14:10 -0400

    Log file change

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-LOGFILE='/tmp/debug.log'
-#LOGFILE='/dev/null'
+LOGFILE='/home/pi/your-face-masterpiece.log'
 
 if [ `/usr/bin/git diff origin | wc -l` -gt 2 ]; then
   cd /home/pi/your-face-masterpiece && /usr/bin/git pull >> $LOGFILE
