4999b3e9ecfd0b147b24952ef8c5aba2d34be41b
Houssam Haidar <houssam@sdelements.com>
2014-04-08 22:00:53 -0400

    Add keyboard controls

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,11 +17,56 @@ angular.module('yourFace')
     .controller('yourFaceController', function($scope, $interval, peopleFactory) {
         $scope.people = peopleFactory;
         $scope.index = 0;
-        $interval(function() {
+        $('body').keydown(function(e) {
+            switch(e.keyCode) {
+                case 80:
+                    $scope.toggle();
+                    break;
+                case 37:
+                    $scope.stop();
+                    $scope.previous();
+                    break;
+                case 39:
+                    $scope.stop();
+                    $scope.next();
+                    break;
+            }
+            $scope.$apply();
+        });
+        $scope.start = function() {
+            $scope.stop();
+            $scope.timer = $interval(function() {
+                $scope.next();
+            }, 7 * 1000);
+        };
+        $scope.stop = function() {
+            if ($scope.timer) {
+                $interval.cancel($scope.timer);
+            }
+            $scope.timer = null;
+        };
+        $scope.toggle = function() {
+            if ($scope.timer) {
+                $scope.stop();
+                console.log('stopped')
+            } else {
+                $scope.start();
+                console.log('started')
+            }
+        }
+        $scope.previous = function() {
+            if ($scope.index > 0){
+                $scope.index--;
+            } else {
+                $scope.index = $scope.people.length - 1;
+            }
+        }
+        $scope.next = function() {
             if ($scope.index < $scope.people.length - 1){
                 $scope.index++;
             } else {
                 $scope.index = 0;
             }
-        }, 7 * 1000)
+        }
+        $scope.start();
     });
