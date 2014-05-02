d1d99875da6ff953e19f12cb290fb28fea5ee689
Houssam Haidar <houssam@sdelements.com>
2014-05-02 17:51:03 -0400

    Wat

--- assets/js/data/people.js
+++ assets/js/data/people.js
@@ -2,53 +2,12 @@
     window.data = window.data || {};
     window.data.people = [
         {
-            "id": "wolever",
-            "firstname": "David",
-            "lastname": "Wolever",
-            "title": "Director, PyCon Canada",
-            "twitter": "@wolever",
-            "github": "wolever",
-            "picture": "david.jpg"
-        },
-        {
-            "id": "haidar",
-            "firstname": "Houssam",
-            "lastname": "Haidar",
-            "title": "Chicken dinner or somethin'",
-            "twitter": "@letstweetbro",
-            "github": "hhaidar",
-            "picture": "houssam.jpg"
-        },
-        {
-            "id": "jarl",
-            "firstname": "Jarl",
-            "lastname": "Stefansson",
-            "title": "Dev-ops Jesus",
-            "github": "jalli",
-            "picture": "jarl.jpg",
-            "scale": {
-                "lastname": 0.7
-            }
-        },
-        {
-            "id": "ram",
-            "firstname": "Ramanan",
-            "lastname": "Sivaranjan",
-            "title": "Lead Developer, SDE",
-            "github": "funkaoshi",
-            "twitter": "funkaoshi",
-            "picture": "ram.jpg",
-            "scale": {
-                "lastname": 0.7
-            }
-        },
-        {
-            "id": "anonymoose",
-            "firstname": "Anony",
-            "lastname": "Moose",
-            "title": "Who's asking?",
-            "github": "???",
-            "twitter": "???"
+            "id": "saucebaus",
+            "firstname": "Saurabh",
+            "lastname": "Harit",
+            "title": "Do you even lift?",
+            "twitter": "@0xsauby",
+            "picture": "saurabh.jpg"
         }
     ];
 })(window);
